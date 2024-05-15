179bc42865542e88dfb008db6f2f222ffab20b6e
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2024-05-15 10:26:30 +0100

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDom1OPzxSBSPV1BZVw0Ar8ZrGmmbw10Sc",
-  authDomain: "moviebooktracker.firebaseapp.com",
-  projectId: "moviebooktracker",
-  storageBucket: "moviebooktracker.appspot.com",
-  messagingSenderId: "418149547452",
-  appId: "1:418149547452:web:eb1074043c44856c29ffb9",
+  apiKey: "your_key",
+  authDomain: "your_domain.firebaseapp.com",
+  projectId: "project_name",
+  storageBucket: "project_name.appspot.com",
+  messagingSenderId: "your_id",
+  appId: "your_appID",
 };
 
 // Initialize Firebase
